96a1cb1686cb194ec4d6dc95d7a14d4a1490b6a7
CWestICL <cwest@ic.ac.uk>
2022-07-07 14:35:04 +0100

    Changes made

--- challenges/caesar.js
+++ challenges/caesar.js
@@ -21,7 +21,29 @@ Use `npm run test:caesar` in the terminal to test your solutions
  * @returns {string} the encrypted text in UPPERCASE
  */
 function caesarEncode (msg, key) {
-  // write a function to encode the msg using the caesar cyhper with given key and return it in UPPERCASE
+  const alph = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+  let alphCap = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+  let newMsg = "";
+
+  for (i = 0; i < key; i++) {
+    let temp = alphCap.shift();
+    alphCap.push(temp);
+  }
+
+  //console.log(alphCap);
+  
+  for (i = 0; i < msg.length; i++) {
+    for (x = 0; x < alph.length; x++) {
+      if (msg[i] === alph[x]) {
+        newMsg += alphCap[x];
+      }
+    }
+    if (msg[i] === " ") {
+      newMsg += " ";
+    }
+  }
+  return newMsg;
+  
 }
 
 /**
@@ -31,7 +53,31 @@ function caesarEncode (msg, key) {
  * @returns {string} the decrypted (original) text in lowercase
  */
 function caesarDecode (cyp, key) {
-  // write a function which decrypts the cyp into lowercase
+  const alphCap = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+  let alph = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+  let newCyp = "";
+
+  console.log("Key:",key);
+
+  for (i = 0; i < key; i++) {
+    let temp = alph.pop();
+    console.log("Temp:",temp);
+    alph.unshift(temp);
+  }
+
+  console.log(alph);
+  
+  for (i = 0; i < cyp.length; i++) {
+    for (x = 0; x < alphCap.length; x++) {
+      if (cyp[i] === alphCap[x]) {
+        newCyp += alph[x];
+      }
+    }
+    if (cyp[i] === " ") {
+      newCyp += " ";
+    }
+  }
+  return newCyp;
 }
 
 module.exports = { caesarEncode, caesarDecode }
